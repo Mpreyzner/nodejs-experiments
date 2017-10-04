a64f5b04f94f5dd9dfbf4195c92bcd4cff088d54
Mpreyzner <maria.preyzner@gmail.com>
2017-10-04 14:54:01 +0200

    Code cleanup

--- node_experiments.js
+++ node_experiments.js
@@ -1,17 +1,14 @@
-//sync
-
 const fs = require('fs');
 
 function sync() {
-    let processId;
-
+    
     let filenames = fs.readdirSync(".")
     for (let i = 0; i < filenames.length; i++) {
         console.log(filenames[i]);
     }
 
     console.log("Sync Ready.")
-    processId = process.getuid();
+   let processId = process.getuid();
 }
 
 function asynchro() {
@@ -24,7 +21,7 @@ function asynchro() {
         console.log("Async Ready.")
     });
 
-    processId = process.getuid();
+   let processId = process.getuid();
 }
 
 sync()
