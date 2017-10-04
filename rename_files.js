0fea993d54847b928f08df9e58a3fc2bd03ce2b0
Mpreyzner <maria.preyzner@gmail.com>
2017-10-04 15:48:01 +0200

    add async renaming file

--- rename_files.js
+++ rename_files.js
@@ -14,4 +14,21 @@ function sync() {
     console.log(isSymLink);
 }
 
+function asynchro() {
+    // body...
+}
+
 sync()
+
+
+let oldFilename = "./processId.txt"
+let newFilename = "./processIdOld.txt"
+
+fs.chmod(oldFilename, 777, function (err) {
+    fs.rename(oldFilename, newFilename, function (err) {
+        fs.lstat(newFilename, function (err, stats) {
+            let isSymLink = stats.isSymbolicLink();
+            console.log(isSymLink);
+        });
+    })
+})
