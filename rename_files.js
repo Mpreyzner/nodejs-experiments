1f1678ce0f01f44835a7578012fdfc158cff3935
Mpreyzner <maria.preyzner@gmail.com>
2017-10-04 15:34:34 +0200

    add renaming file sync

---
+++ rename_files.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+
+let oldFilename = "./processId.txt"
+let newFilename = "./processIdOld.txt"
+
+fs.writeFileSync(oldFilename, 'test')
+
+fs.chmodSync(oldFilename, 777);
+fs.renameSync(oldFilename, newFilename);
+
+let isSymLink = fs.lstatSync(newFilename).isSymbolicLink();
+console.log(isSymLink);
