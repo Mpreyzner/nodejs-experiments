40a0c2e95fa55d4fbcc3276eeb156c39eabf4453
Mpreyzner <maria.preyzner@gmail.com>
2017-10-04 15:39:45 +0200

    extract to function

--- rename_files.js
+++ rename_files.js
@@ -1,13 +1,17 @@
 const fs = require('fs')
 
 
-let oldFilename = "./processId.txt"
-let newFilename = "./processIdOld.txt"
+function sync() {
+    let oldFilename = "./processId.txt"
+    let newFilename = "./processIdOld.txt"
 
-fs.writeFileSync(oldFilename, 'test')
+    fs.writeFileSync(oldFilename, 'test')
 
-fs.chmodSync(oldFilename, 777);
-fs.renameSync(oldFilename, newFilename);
+    fs.chmodSync(oldFilename, 777);
+    fs.renameSync(oldFilename, newFilename);
 
-let isSymLink = fs.lstatSync(newFilename).isSymbolicLink();
-console.log(isSymLink);
+    let isSymLink = fs.lstatSync(newFilename).isSymbolicLink();
+    console.log(isSymLink);
+}
+
+sync()
