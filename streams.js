c24d01170a9bb4e41948bb5ff77a4720bf80c2a0
Mpreyzner <maria.preyzner@gmail.com>
2017-10-05 21:54:18 +0200

    add more logging

--- streams.js
+++ streams.js
@@ -1,11 +1,19 @@
 const request = require('request');
 
 let stream = request('http://www.pluralsight.com/');
-
+let counter = 0;
 stream.on('data', (chunk) => {
-	console.log(">>>Data>>>>>" + chunk);
+	console.log(">>>Data>>>>>" + counter);
+	counter++;
 })
+console.log('Am I waiting or not?');
+
+setTimeout(() => {
+  console.log('hello again');
+}, 1000);
 
 stream.on('end', () => {
 	console.log(">>>>Done>>>>");
 })
+
+console.log('This is the end. My only friend. The end.');
